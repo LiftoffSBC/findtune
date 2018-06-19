2799dd8e71e5a1131caca9ebe5de3b163fbfb687
Missy Norman <missy.norman412@gmail.com>
2018-06-19 18:30:52 -0400

    code that gets lyrics

--- assets/javascript/musixmatch.js
+++ assets/javascript/musixmatch.js
@@ -1,21 +1,45 @@
-jQuery.ajaxPrefilter(function(options) {
+jQuery.ajaxPrefilter(function (options) {
     if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://uncc-cors-proxy.herokuapp.com/' + options.url;
+      options.url = 'https://uncc-cors-proxy.herokuapp.com/' + options.url;
     }
-});
-jQuery.ajaxSettings.traditional = true;
+  });
+  jQuery.ajaxSettings.traditional = true;
+  // https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=101516356&apikey=27b1b9c9a65fd7e140176cc0d63d7304
 
-queryURL = "https://api.musixmatch.com/ws/1.1/track.search?q_track=%22Layla%22&apikey=27b1b9c9a65fd7e140176cc0d63d7304";
+  // https://api.musixmatch.com/ws/1.1/track.search?q_track=%22Layla%22&apikey=27b1b9c9a65fd7e140176cc0d63d7304
+  var searchURL =
+    "https://api.musixmatch.com/ws/1.1/track.search?q_track=%22Layla%22&"
+  var lyricURL = "https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id="
 
+  var key = "apikey=27b1b9c9a65fd7e140176cc0d63d7304"
+  $.ajax({
+    // Go get the song details
+    url: searchURL + key,
+    method: "GET"
+  }).done(function (response) {
+    var parsed = JSON.parse(response);
+    var myTracks = parsed.message.body.track_list
+    console.log(JSON.stringify(myTracks, null, 4, ));
+    // now go get the lyrics
+    for (var i = 0; i < myTracks.length; i++) {
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function (response) {
-  //console.log(queryURL)
-  // Printing the entire object to console
-  console.log(response);
-  //get the track id
-  var trackID = response.message.body.track_list[0].track.track_id;
-  console.log(trackID);
-})
+      var songID = myTracks[i].track.track_id.toString() + "&";
+      getLyrics(songID);
+
+    }
+  });
+
+  function getLyrics(id) {
+    console.log("URL = " + lyricURL + id + key);
+    $.ajax({
+      // Go get the song details
+      url: lyricURL + id + key,
+      method: "GET"
+    }).done(function (response) {
+      var parsed = JSON.parse(response);
+      console.log(parsed);
+      var lyrics = parsed.message.body.lyrics.lyrics_body
+      console.log(lyrics)
+
+    });
+  }
