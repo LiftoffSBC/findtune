b475c179a2bdeef86aa787cf7597e60d16691a70
Austin Dailly <austin@wifi_stu-10-38-84-172.uncc.edu>
2018-06-21 10:33:59 -0400

    Updated styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,22 @@
-var queryURL = "https://api.napster.com/v2.2/artists/u2?apikey=OTZjM2VhNDgtNTRkNi00ZGI0LWFhYWItOWJjYjhlMGQzODg5";
-
+var apiKey = "OTZjM2VhNDgtNTRkNi00ZGI0LWFhYWItOWJjYjhlMGQzODg5";
+var bandName = "the beatles"; // band name to pass through
+var fixedBandName = bandName.split(' ').join('-');
+var queryURL = "https://api.napster.com/v2.2/artists/" + fixedBandName + "?apikey=" + apiKey;
+var bands = [];
+console.log(queryURL)
+var images = [];
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function (response) {
   //console.log(queryURL)
-  // Printing the entire object to console
+  // Printing the entire object to console 
   //console.log(response);
   var artistID = response.artists[0].id;
-  var queryURL = 'https://api.napster.com/v2.2/artists/' + artistID + '/similar?apikey=OTZjM2VhNDgtNTRkNi00ZGI0LWFhYWItOWJjYjhlMGQzODg5';
+  var bio = response.artists[0].blurbs[0]; //artist bio
+  console.log(bio);
+  var queryURL = 'https://api.napster.com/v2.2/artists/' + artistID + '/similar?apikey=' + apiKey;
   //console.log(queryURL);
   $.ajax({
     url: queryURL,
@@ -18,12 +25,45 @@ $.ajax({
   }).then(function (response) {
     //console.log(queryURL)
 
-    //console.log(response);
+    console.log(response);
     //print out 10 similar artists
     for (var i = 0; i < 10; i++) {
-      console.log(response.artists[i].name);
+      images[i] = response.artists[i].links.images; // similar artist image url
+      console.log(images[i]);
     }
 
   })
+  //console.log(queryURL);
+  var queryURL = 'https://api.napster.com/v2.2/artists/' + artistID + '/images?apikey=' + apiKey;
+  //console.log(queryURL);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+
+  }).then(function (response) {
+    //console.log(queryURL)
+
+    console.log(response);
+    //the image is the url of the first image grabbed. image of the current artist
+    image = response.images[0].url; // current artist image
+    console.log(image);
+    $("#artist-image").append(image);
+    // current artist name
+
+
+  });
+
 })
+// submit button show/hide results div and take search-input
+$("#submit-button").on("click", function (event) {
+  event.preventDefault();
+  var search = $("#search-input").val().trim()
+  if (search === '' || search === undefined || search === null) {
+    return;
+  }
+  bands.push(search);
+  console.log(search);
+  $("#results").show();
+  $("#main").hide();
+});
 
